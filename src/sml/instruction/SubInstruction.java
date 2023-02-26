36475e7bc8544505acdb61930ed96e9d02d15c5a
chm1997 <calynhughesmcinnes@gmai.com>
2023-02-26 12:55:43 +0000

    Implement changes in SubInstruction in order to pass tests

--- src/sml/instruction/SubInstruction.java
+++ src/sml/instruction/SubInstruction.java
@@ -15,7 +15,7 @@ public class SubInstruction extends Instruction {
         private final RegisterName result;
         private final RegisterName source;
 
-        public static final String OP_CODE = "add";
+        public static final String OP_CODE = "sub";
 
         public SubInstruction(String label, RegisterName result, RegisterName source) {
                 super(label, OP_CODE);
@@ -27,7 +27,7 @@ public class SubInstruction extends Instruction {
         public int execute(Machine m) {
                 int value1 = m.getRegisters().get(result);
                 int value2 = m.getRegisters().get(source);
-                m.getRegisters().set(result, value1 + value2);
+                m.getRegisters().set(result, value1 - value2);
                 return NORMAL_PROGRAM_COUNTER_UPDATE;
         }
 
@@ -43,6 +43,6 @@ public class SubInstruction extends Instruction {
 
         @Override
         public int hashCode() {
-                return 111;
+                return 112;
         }
 }
