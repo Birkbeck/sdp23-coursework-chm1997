2179c7a186030579721e9f9c03f118987370a860
chm1997 <calynhughesmcinnes@gmai.com>
2023-02-26 13:35:18 +0000

    Implemented DivInstructionTests and dummy class in order for code to compile

---
+++ test/sml/instruction/DivInstructionTest.java
@@ -0,0 +1,132 @@
+package sml.instruction;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
+
+import static sml.Registers.Register.*;
+
+class DivInstructionTest {
+    private Machine machine;
+    private Registers registers;
+
+    @BeforeEach
+    void setUp() {
+        machine = new Machine(new Registers());
+        registers = machine.getRegisters();
+    }
+
+    @AfterEach
+    void tearDown() {
+        machine = null;
+        registers = null;
+    }
+
+    @Test
+    void executeValid() {
+        registers.set(EAX, 30);
+        registers.set(EBX, 6);
+        Instruction instruction = new DivInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        Assertions.assertEquals(5, machine.getRegisters().get(EAX));
+    }
+
+    @Test
+    void executeValidTwo() {
+        registers.set(EAX, -30);
+        registers.set(EBX, 5);
+        Instruction instruction = new DivInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        Assertions.assertEquals(-6, machine.getRegisters().get(EAX));
+    }
+
+    @Test
+    void executeValidThree() {
+        registers.set(EAX, 5);
+        registers.set(EBX, 2);
+        Instruction instruction = new DivInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        Assertions.assertEquals(2, machine.getRegisters().get(EAX));
+    }
+
+    @Test
+    void executeValidFour() {
+        registers.set(EAX, 5);
+        registers.set(EBX, 0);
+        Instruction instruction = new DivInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        Assertions.assertEquals(0, machine.getRegisters().get(EAX));
+    }
+
+    @Test
+    void executeValidFive() {
+        registers.set(EAX, 0);
+        registers.set(EBX, 5);
+        Instruction instruction = new DivInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        Assertions.assertEquals(0, machine.getRegisters().get(EAX));
+    }
+
+    @Test
+    void toStringValid(){
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        Instruction instruction = new DivInstruction(null, EAX, EBX);
+        String ActualString = instruction.toString();
+        String ExpectedString = "div EAX EBX";
+        Assertions.assertEquals(ExpectedString, ActualString);
+    }
+
+    @Test
+    void toStringValidLabel(){
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        Instruction instruction = new DivInstruction("f1", EAX, EBX);
+        String ActualString = instruction.toString();
+        String ExpectedString = "f1: div EAX EBX";
+        Assertions.assertEquals(ExpectedString, ActualString);
+    }
+
+    @Test
+    void equalsValid(){
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        registers.set(ECX, 4);
+        registers.set(EDX, 7);
+        Instruction instruction1 = new DivInstruction(null, EAX, EBX);
+        Instruction instruction2 = new DivInstruction(null, ECX, EDX);
+        Assertions.assertTrue(instruction1.equals(instruction2));
+    }
+
+    @Test
+    void equalsInvalid(){
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        registers.set(ECX, 4);
+        registers.set(EDX, 7);
+        Instruction instruction1 = new DivInstruction(null, EAX, EBX);
+        Instruction instruction2 = new MulInstruction(null, ECX, EDX);
+        Assertions.assertFalse(instruction1.equals(instruction2));
+    }
+
+    @Test
+    void hashCodeValid() {
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        Instruction instruction1 = new AddInstruction(null, EAX, EBX);
+        Assertions.assertEquals(114, instruction1.hashCode());
+    }
+
+    @Test
+    void hashCodInvalid() {
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        Instruction instruction1 = new AddInstruction(null, EAX, EBX);
+        Assertions.assertNotEquals(1112321312, instruction1.hashCode());
+    }
+}
