000872f295f25ebd991730894f475fe94be53690
chm1997 <calynhughesmcinnes@gmai.com>
2023-02-26 13:02:05 +0000

    Implemented MulInstruction code in order to pass tests

--- src/sml/instruction/MulInstruction.java
+++ src/sml/instruction/MulInstruction.java
@@ -5,8 +5,8 @@ import sml.Machine;
 import sml.RegisterName;
 
 /**
- * This class represents a machine instruction for addition.
- * It adds the contents of two registers given on instancing the object.
+ * This class represents a machine instruction for multiplication.
+ * It multiplies the contents of two registers given on instancing the object.
  * It stores the results of the operation in the first register.
  * @author Calyn Hughes McInnes (chm1997)
  */
@@ -15,7 +15,7 @@ public class MulInstruction extends Instruction {
     private final RegisterName result;
     private final RegisterName source;
 
-    public static final String OP_CODE = "sub";
+    public static final String OP_CODE = "mul";
 
     public MulInstruction(String label, RegisterName result, RegisterName source) {
         super(label, OP_CODE);
@@ -27,7 +27,7 @@ public class MulInstruction extends Instruction {
     public int execute(Machine m) {
         int value1 = m.getRegisters().get(result);
         int value2 = m.getRegisters().get(source);
-        m.getRegisters().set(result, value1 - value2);
+        m.getRegisters().set(result, value1 * value2);
         return NORMAL_PROGRAM_COUNTER_UPDATE;
     }
 
@@ -43,6 +43,6 @@ public class MulInstruction extends Instruction {
 
     @Override
     public int hashCode() {
-        return 112;
+        return 113;
     }
 }
